2079fc2315814a02e29944374330cea6e6898de2
Kevin Mendez <kevinrmendez@gmail.com>
2021-08-27 19:30:14 +0300

    implement connectivity plugin into the app to check for internet connection before starting order

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Buutti Ravintola
 
 Buutti Ravintola is an app written for buutti codematch. The app is written in flutter and uses firebase (firestore) for the backend of the app. 
-The app implements [bloc](https://pub.dev/packages/bloc) state management. The app logic was tested using [bloc test](https://pub.dev/packages/bloc_test) for performing unit tests.  The app works for both android and ios. 
+The app implements [bloc](https://pub.dev/packages/bloc) state management. The app logic was tested using [bloc test](https://pub.dev/packages/bloc_test) for performing unit tests.  The app works for both android and ios. Make sure the app is connected to the internet (wifi/mobile data) before using the app.
 The structure of the project is the following:
 - screens 
 - widgets
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,14 @@
-import 'package:buutti_ravintola/screens/screens.dart';
+import 'dart:async';
+
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../blocs/cart/cart.dart';
 import '../widgets/buutti_logo.dart';
 import '../widgets/primary_button.dart';
+import 'screens.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,7 +18,58 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  double targetValue = 150;
+  final Connectivity _connectivity = Connectivity();
+  String _connectionStatusErrorMessage = 'Unknown';
+  late ConnectivityResult _connectionStatus;
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  Future<void> initConnectivity() async {
+    ConnectivityResult result = ConnectivityResult.none;
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    switch (result) {
+      case ConnectivityResult.wifi:
+      case ConnectivityResult.mobile:
+      case ConnectivityResult.none:
+        setState(() {
+          _connectionStatus = result;
+          _connectionStatusErrorMessage = result.toString();
+        });
+        break;
+      default:
+        setState(() {
+          _connectionStatusErrorMessage = 'Failed to get connectivity.';
+        });
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     CartBloc _cartBloc = BlocProvider.of(context);
@@ -42,12 +97,36 @@ class _HomeScreenState extends State<HomeScreen> {
               PrimaryButton(
                 text: 'start order',
                 onPressed: () {
-                  _cartBloc.add(const DeleteAllMenuItem());
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MainDishScreen()),
-                  );
+                  print('connectivity');
+                  print(_connectionStatus == ConnectivityResult.none);
+                  if (_connectionStatus == ConnectivityResult.none) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("No internet detected"),
+                          content: Text(
+                              "Error: $_connectionStatusErrorMessage Before starting an order, make sure your app is connected to the internet."),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text("OK"),
+                            ),
+                          ],
+                        );
+                        ;
+                      },
+                    );
+                  } else {
+                    _cartBloc.add(const DeleteAllMenuItem());
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MainDishScreen()),
+                    );
+                  }
                 },
               )
             ],
